b2f0338f8ef72ee4cb0aafb96a332ad1e231e547
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-15 21:25:41 -0700

    before moving loading shop data into redux shop reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils.js';
-
-import { updateCollections } from '../../redux/shop/shop.actions';
+//import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils.js';
+//import { updateCollections } from '../../redux/shop/shop.actions';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -23,8 +22,8 @@ class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections');
+    //const { updateCollections } = this.props;
+    //const collectionRef = firestore.collection('collections');
 
     // (1) using observables to get data
     //this.unsubscribeFromSnapshot = collectionRef.onSnapshot( async snapshot => {
@@ -34,11 +33,11 @@ class ShopPage extends React.Component {
     //}); 
 
     // Using promise to get the data
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
+    //collectionRef.get().then(snapshot => {
+    //  const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //  updateCollections(collectionsMap);
+    //  this.setState({ loading: false });
+    //});
   }
 
   componentWillUnmount() {
@@ -74,10 +73,10 @@ class ShopPage extends React.Component {
   };
 } 
 
-
-const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))
-});
+// MOVED fetching data into shop reducer
+//const mapDispatchToProps = dispatch => ({
+//  updateCollections: collectionsMap =>
+//    dispatch(updateCollections(collectionsMap))
+//});
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,37 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
+import { 
+  firestore, 
+  convertCollectionsSnapshotToMap 
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsSuccess = collectionMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+  payload: collectionMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    
+    dispatch(fetchCollectionsStart());
+
+    // Using promise to get the data asychronously
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  }
+};
